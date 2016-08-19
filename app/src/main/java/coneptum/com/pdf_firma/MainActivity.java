bd98a82ab67025a8514bd64967974f8c312f5714
JessPG <jessica.perea@coneptum.com>
2016-08-19 11:34:35 +0200

    funcional

--- app/src/main/java/coneptum/com/pdf_firma/MainActivity.java
+++ app/src/main/java/coneptum/com/pdf_firma/MainActivity.java
@@ -2,12 +2,75 @@ package coneptum.com.pdf_firma;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.shockwave.pdfium.PdfDocument;
+
+import java.util.List;
+
+import coneptum.com.android_pdf_viewer.PDFView;
+import coneptum.com.android_pdf_viewer.listener.OnLoadCompleteListener;
+import coneptum.com.android_pdf_viewer.listener.OnPageChangeListener;
+
+public class MainActivity extends AppCompatActivity implements OnPageChangeListener, OnLoadCompleteListener {
+
+    private static final String SAMPLE_FILE = "sample.pdf";
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private String pdfFileName;
+    private int pageNumber = 0;
+    private PDFView pdfView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.pdfView = (PDFView) findViewById(R.id.pdfView);
+        displayFromAsset(SAMPLE_FILE);
+    }
+
+    private void displayFromAsset(String assetFileName) {
+        pdfFileName = assetFileName;
+
+        pdfView.fromAsset(SAMPLE_FILE)
+                .defaultPage(pageNumber)
+                .onPageChange(this)
+                .enableAnnotationRendering(true)
+                .onLoad(this)
+                //.scrollHandle(new DefaultScrollHandle(this))
+                .load();
+    }
+
+    @Override
+    public void loadComplete(int nbPages) {
+        PdfDocument.Meta meta = pdfView.getDocumentMeta();
+        Log.e(TAG, "title = " + meta.getTitle());
+        Log.e(TAG, "author = " + meta.getAuthor());
+        Log.e(TAG, "subject = " + meta.getSubject());
+        Log.e(TAG, "keywords = " + meta.getKeywords());
+        Log.e(TAG, "creator = " + meta.getCreator());
+        Log.e(TAG, "producer = " + meta.getProducer());
+        Log.e(TAG, "creationDate = " + meta.getCreationDate());
+        Log.e(TAG, "modDate = " + meta.getModDate());
+
+        printBookmarksTree(pdfView.getTableOfContents(), "-");
+
+    }
+
+    public void printBookmarksTree(List<PdfDocument.Bookmark> tree, String sep) {
+        for (PdfDocument.Bookmark b : tree) {
+
+            Log.e(TAG, String.format("%s %s, p %d", sep, b.getTitle(), b.getPageIdx()));
+
+            if (b.hasChildren()) {
+                printBookmarksTree(b.getChildren(), sep + "-");
+            }
+        }
+    }
+
+    @Override
+    public void onPageChanged(int page, int pageCount) {
+        pageNumber = page;
     }
 }

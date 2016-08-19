03d6e18aeda3067b7771d289689ffc0bd063ff92
JessPG <jessica.perea@coneptum.com>
2016-08-19 10:55:17 +0200

    init

---
+++ app/src/main/java/coneptum/com/pdf_firma/MainActivity.java
@@ -0,0 +1,13 @@
+package coneptum.com.pdf_firma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
